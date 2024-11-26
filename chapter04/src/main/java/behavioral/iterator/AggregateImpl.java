c0be47293a06ca7ad643a740876d2af39cec0174
minseokim0629 <han8121449@naver.com>
2024-11-26 11:08:30 +0900

    241126_2 오류 관련 주석 추가

--- chapter04/src/main/java/behavioral/iterator/AggregateImpl.java
+++ chapter04/src/main/java/behavioral/iterator/AggregateImpl.java
@@ -12,6 +12,7 @@ public class AggregateImpl<E> implements Aggregate<E> {
 		return new IteratorImpl();
 	}
 	
+	//AgreegateImpl<E> 타입으로 선언했기 떄문에 IteratorImpl<E>로 하면 에러남 <T>로 해주거나 아님 지우거나
 	private class IteratorImpl implements Iterator<E>{
 		private int index = 0;
 		
